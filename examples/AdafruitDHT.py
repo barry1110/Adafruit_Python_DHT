71065e3dfa616a739b077a99aa1d7795c1e01963
barry1110 <56903178+barry1110@users.noreply.github.com>
2020-11-16 14:04:52 +0800

    Update AdafruitDHT.py

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -26,6 +26,25 @@ import time
 import sys  
 import httplib, urllib  
 import json
+deviceId = "DhkaAAbg “
+deviceKey = “XdiXYDbzxNA6mRK5“ 
+def post_to_mcs(payload): 
+	headers = {"Content-type": "application/json", "deviceKey": deviceKey} 
+	not_connected = 1 
+	while (not_connected):
+		try:
+			conn = http.HTTPConnection("api.mediatek.com:80")
+			conn.connect() 
+			not_connected = 0 
+		except (http.HTTPException, socket.error) as ex: 
+			print ("Error: %s" % ex)
+ 			time.sleep(10)
+			 # sleep 10 seconds 
+	conn.request("POST", "/mcs/v2/devices/" + deviceId + "/datapoints", json.dumps(payload), headers) 
+	response = conn.getresponse() 
+	print( response.status, response.reason, json.dumps(payload), time.strftime("%c")) 
+	data = response.read() 
+	conn.close() 
 
 # Parse command line parameters.
 sensor_args = { '11': Adafruit_DHT.DHT11,
